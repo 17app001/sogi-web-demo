baa2aef458559385865c525dd582fa08f25fe726
jerrychen <jerry@gmail.com>
2022-09-14 20:37:55 +0800

    完成基礎邏輯

--- js/index.js
+++ js/index.js
@@ -2,7 +2,44 @@ const phoneEl = document.querySelector("#phone-number");
 const analyticsEl = document.querySelector("#analytics");
 console.log(phoneEl, analyticsEl);
 
+phoneEl.value = "0988883456";
+
 function analytics() {
     let phoneNumber = phoneEl.value;
+    if (phoneNumber == "") {
+        alert("請輸入手機號碼");
+        return;
+    }
+
+    if (phoneNumber.length < 10) {
+        alert("手機號碼為10個數字!");
+        return;
+    }
+
+    //  使用正則式
+    const re = /09\d{8}/;
+    if (!re.test(phoneNumber)) {
+        alert("手機號碼格式不正確!");
+        return;
+    }
+
+
     console.log(phoneNumber);
+    analyticsPhoneNumber(phoneNumber);
+}
+
+
+// 0968168168
+function analyticsPhoneNumber(phoneNumber) {
+    let fourNumber = phoneNumber.substring(phoneNumber.length - 4, phoneNumber.length);
+    console.log(fourNumber);
+    // 取得除以80的結果
+    console.log(fourNumber / 80);
+    // 取得整數部分
+    console.log(parseInt(fourNumber / 80));
+    // (除以80的結果-整數部分)*80
+    let code = parseInt((fourNumber / 80 - parseInt(fourNumber / 80)) * 80);
+    console.log(code);
+
+    return code;
 }
